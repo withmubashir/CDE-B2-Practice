4cf2e390e1b3c8ca74b46f02feddfb5c79ed5875
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-02-19 01:53:09 +0500

    update

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
 print("hello saylani team")
+### Variables
+# - Variable name can't start with number
+# - Variaable name can't have spaces
+# - Can use underscore "_" instad of spaces
+# - Can't use any character expect "_"
+# - Use precise(meaningful) and valid names
+#### NOTE: Can't use the reserve words for variable name
+Name = "Mubashir Hussain"   #string type variable declare
+age = 16                    #number type variable declare
+weight = 32.5               #floating type variable declare
+relationshipStatus = False  #Boolean type variable declare
+print(Name)
+print(type(Name))
+print(age)
+print(type(age))
+print(weight)
+print(type(weight))
+print(relationshipStatus)
+print(type(relationshipStatus))
