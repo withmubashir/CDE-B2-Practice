b6ece08295ffefc265dc887a26b344809d64aa5c
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-01 21:15:13 +0500

    Practice

--- hello.py
+++ hello.py
@@ -28,9 +28,9 @@ print(number)
 
 full_name = "Mubashir Hussain"
 surname = "Saleh"
-print(full_name+surname)
 
 print("{0} {1}".format(full_name, surname))
+print(full_name+surname)
 print(full_name +" "+surname)
 
 print(Name, full_name, surname, age, weight)
