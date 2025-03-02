80cea59ccd93b35eb283799875653f3260d00e56
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-02 12:29:53 +0500

    Practice

--- 01 Python/Class 2/practice.py
+++ 01 Python/Class 2/practice.py
@@ -2,3 +2,20 @@
 print(45 != 54) 
 print(True and False) #and
 print(True or False) #or
+
+#List adding and changing elements
+countries = [
+"Pakistan",
+"Afghanistan",
+"China",
+"India"
+]
+countries = {
+cunt1 = "Pakistan",
+cunt2 = "Afghanistan",
+cunt3 = "China",
+cunt4 = "India"
+}
+
+
+
