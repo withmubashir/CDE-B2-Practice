2af5d267185c85157c0e614cac35cf761cc292c2
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-05 01:28:06 +0500

    practice

--- 01 Python/Class 2/practice.py
+++ 01 Python/Class 2/practice.py
@@ -6,16 +6,14 @@ print(True or False) #or
 #List adding and changing elements
 countries = [
 "Pakistan",
-"Afghanistan",
-"China",
-"India"
+    "China",
+    "Afghanistan"
 ]
-countries = {
-cunt1 = "Pakistan",
-cunt2 = "Afghanistan",
-cunt3 = "China",
-cunt4 = "India"
-}
+
+
+
+
+
 
 
 
