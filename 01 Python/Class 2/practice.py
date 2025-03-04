887b420c93cd28b96ff941cb59458b9320dfce0e
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-05 01:34:12 +0500

    practice

--- 01 Python/Class 2/practice.py
+++ 01 Python/Class 2/practice.py
@@ -10,13 +10,15 @@ countries = [
     "Afghanistan"
 ]
 
+print(type(countries))
 
 print(countries[0])
 print(countries[2])
 print(countries[1])
 
+countries.append("India")
 
-
+print(countries)
 
 
 
