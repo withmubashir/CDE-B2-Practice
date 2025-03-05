e78a57e1332c1f9d88d7de3dbc3c11566f3a6efe
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-06 01:43:22 +0500

    Practice

--- 01 Python/Class 2/practice.py
+++ 01 Python/Class 2/practice.py
@@ -83,3 +83,20 @@ if age >= 18:
     print("You Are Eligible")
 else:
     print("not eligible")
+
+marks = int(input("Enter your Marks"))
+
+if marks > 80 and marks <= 100:
+    print("Grade A")
+elif marks > 60 and marks <= 80:
+    print("Grade B")
+elif marks > 50 and marks <= 60:
+    print("Grade C")
+elif marks > 45 and marks <= 50:
+    print("Grade D")
+elif marks > 25 and marks <= 45:
+    print("Grade E")
+elif marks > 0 and marks <= 25:
+    print("Grade F")
+else:
+    print("Invalid Input")
