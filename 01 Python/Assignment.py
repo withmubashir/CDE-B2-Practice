56d13a55854acf2abda4f125895703a1bf7b3746
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-07 16:53:13 +0500

    Practice

---
+++ 01 Python/Assignment.py
@@ -0,0 +1,23 @@
+print("Name: Mubashir Hussain\nFather Name: Muhammad Saleh\nDate of Birth: 10\\03\\2009")
+
+bio = [
+    "Name: Mubashir Hussain",
+    "F. Name Muhammad Saleh",
+    "Education: Intermediate",
+    "Center: SMIT Zaitoon Ashraf",
+    "Roll Number: 371381"
+]
+
+print(bio)
+
+num = 88
+print(num + 20)
+print(num -20)
+print(num/20)
+print(num*20)
+print(num**2)
+num+=20
+num-=20
+num*=20
+num/=20
+print(num)
