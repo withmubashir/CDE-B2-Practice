f33cdc07fb2ca5657faf80d005f8b2af3711a752
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-13 16:43:08 +0500

    assignment

--- 01 Python/Assignment.py
+++ 01 Python/Assignment.py
@@ -1,63 +1,71 @@
-print("Name: Mubashir Hussain\nFather Name: Muhammad Saleh\nDate of Birth: 10\\03\\2009")
+# print("Name: Mubashir Hussain\nFather Name: Muhammad Saleh\nDate of Birth: 10\\03\\2009")
 
-bio = [
-    "Name: Mubashir Hussain",
-    "F. Name Muhammad Saleh",
-    "Education: Intermediate",
-    "Center: SMIT Zaitoon Ashraf",
-    "Roll Number: 371381"
-]
+# bio = [
+#     "Name: Mubashir Hussain",
+#     "F. Name Muhammad Saleh",
+#     "Education: Intermediate",
+#     "Center: SMIT Zaitoon Ashraf",
+#     "Roll Number: 371381"
+# ]
 
-print(bio)
+# print(bio)
 
-num = 88
-print(num + 20)
-print(num -20)
-print(num/20)
-print(num*20)
-print(num**2)
-num+=20
-num-=20
-num*=20
-num/=20
-print(num)
+# num = 88
+# print(num + 20)
+# print(num -20)
+# print(num/20)
+# print(num*20)
+# print(num**2)
+# num+=20
+# num-=20
+# num*=20
+# num/=20
+# print(num)
 
-english = 55
-islamiat = 67
-math = 100
+# english = 55
+# islamiat = 67
+# math = 100
 
-total_marks = 300
+# total_marks = 300
 
-numbers = english+islamiat+math
-print(numbers/total_marks*100)
+# numbers = english+islamiat+math
+# print(numbers/total_marks*100)
 
-service_year = int(input("enter your years service " ))
-salary = int(input("enter your salary" ))
-if service_year >= 5:
-    bonus = int(salary/20)
-    print("Conratulations! You Got a Bonus of:", bonus)
+# service_year = int(input("enter your years service " ))
+# salary = int(input("enter your salary" ))
+# if service_year >= 5:
+#     bonus = int(salary/20)
+#     print("Conratulations! You Got a Bonus of:", bonus)
+# else:
+#     print("Sorry, you are not eligible for a bonus.")
+
+# age = int(input("Enter Your Age"))
+
+# if age > 17:
+#     print("You're Eligible For Voting")
+# else:
+#     print("You Are Not Eligible For Voting")
+# marks = int(input("Enter your Marks"))
+
+# if marks > 80 and marks <= 100:
+#     print("Grade A")
+# elif marks > 60 and marks <= 80:
+#     print("Grade B")
+# elif marks > 50 and marks <= 60:
+#     print("Grade C")
+# elif marks > 45 and marks <= 50:
+#     print("Grade D")
+# elif marks > 25 and marks <= 45:
+#     print("Grade E")
+# elif marks > 0 and marks <= 25:
+#     print("Grade F")
+# else:
+#     print("Invalid Input")
+
+number = int(input("Write a Number: "))
+remainder = number%2
+
+if remainder == 0:
+    print("This is an Even Number")
 else:
-    print("Sorry, you are not eligible for a bonus.")
-
-age = int(input("Enter Your Age"))
-
-if age > 17:
-    print("You're Eligible For Voting")
-else:
-    print("You Are Not Eligible For Voting")
-marks = int(input("Enter your Marks"))
-
-if marks > 80 and marks <= 100:
-    print("Grade A")
-elif marks > 60 and marks <= 80:
-    print("Grade B")
-elif marks > 50 and marks <= 60:
-    print("Grade C")
-elif marks > 45 and marks <= 50:
-    print("Grade D")
-elif marks > 25 and marks <= 45:
-    print("Grade E")
-elif marks > 0 and marks <= 25:
-    print("Grade F")
-else:
-    print("Invalid Input")
+    print("This is an Odd Number")
