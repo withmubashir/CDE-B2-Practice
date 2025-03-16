68d1519e5eb8e3be2beca8929012c36a799e761a
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-16 23:27:04 +0500

    Updated

--- 01 Python/Assignment.py
+++ 01 Python/Assignment.py
@@ -195,6 +195,7 @@
 # else:
 #     print(f"Total bill amount is Rs.{int(units*10)}")
 
-16) Write a program to check if a year is leap year or not.
+# 16) Write a program to check if a year is leap year or not.
 
-If a year is divisible by 4 then it is leap year but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
+# If a year is divisible by 4 then it is leap year but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
+
