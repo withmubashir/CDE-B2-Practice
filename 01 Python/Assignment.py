d29e6a47aed7375fc4bbbe4c5fae370eaf2704e5
Mubashir Hussain Saleh <114300781+MubashirHussaini@users.noreply.github.com>
2025-03-17 15:42:41 +0500

    Updated

--- 01 Python/Assignment.py
+++ 01 Python/Assignment.py
@@ -198,4 +198,13 @@
 # 16) Write a program to check if a year is leap year or not.
 
 # If a year is divisible by 4 then it is leap year but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
-
+year = int(input("Enter any Year: "))
+new = 0
+if year == 1900 or year == 2000 or year == 2100:
+    new = year%400
+    if new == 0: print("it is leap year")
+    else:print("it is not leap year")
+else:
+    new = year%4
+    if new == 0: print("it is leap year")
+    else:print("it is not leap year")
