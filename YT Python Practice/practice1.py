6e7ff1e018f104a1b8b8d7689bd6365f9e9eb36d
withmubashir <kingmubashir018@gmail.com>
2025-04-11 20:52:22 +0500

    Practice

--- YT Python Practice/practice1.py
+++ YT Python Practice/practice1.py
@@ -3,4 +3,10 @@
 num1 = int(input("Enter a Number: "))
 num2 = int(input("Enter a Number: "))
 
-print("SUM = "num1+num2)
+print("SUM = ",num1+num2)
+
+#WAP to input side of a square & print its area.
+
+side = int(input("Enter one side size of square: "))
+
+print("Total area of square is: ",side*side)
