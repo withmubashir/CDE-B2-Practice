4be456fee12130f780aef8183fe66efa592026c0
withmubashir <kingmubashir018@gmail.com>
2025-04-11 21:05:15 +0500

    practice

--- YT Python Practice/practice1.py
+++ YT Python Practice/practice1.py
@@ -1,21 +1,31 @@
-#Write a Program to input 2 numbers & print their sum.
+# #Write a Program to input 2 numbers & print their sum.
 
-num1 = int(input("Enter a Number: "))
-num2 = int(input("Enter a Number: "))
+# num1 = int(input("Enter a Number: "))
+# num2 = int(input("Enter a Number: "))
 
-print("SUM = ",num1+num2)
+# print("SUM = ",num1+num2)
 
-#WAP to input side of a square & print its area.
+# #WAP to input side of a square & print its area.
 
-side = int(input("Enter one side size of square: "))
+# side = int(input("Enter one side size of square: "))
 
-print("Total area of square is: ",side**2)
+# print("Total area of square is: ",side**2)
 
-#WAP to input 2 floating point numbers & print their average.
+# #WAP to input 2 floating point numbers & print their average.
 
-float1 = float(input("Enter any Float Number: "))
-float2 = float(input("Enter any Float Number: "))
+# float1 = float(input("Enter any Float Number: "))
+# float2 = float(input("Enter any Float Number: "))
 
-print("Average of floating values is: ", (float1+float2)/2)
+# print("Average of floating values is: ", (float1+float2)/2)
 
+"""
+WAP to input 2 int numbers, a and b.
+Print True if a is greater than or equal to b. If not print False.
+"""
 
+a = int(input("Enter any number: "))
+b = int(input("Enter any number: "))
+
+c = a>=b
+
+print(c)
