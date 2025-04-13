aff701f7a5a3756f40a89868f914927dd5639d33
withmubashir <kingmubashir018@gmail.com>
2025-04-13 20:05:57 +0500

    Practice

--- 01 Python/Class 7/practice.py
+++ 01 Python/Class 7/practice.py
@@ -3,6 +3,7 @@
 # sports car -> child
 # family car -> child
 
+# Parent Class
 class car():
     # Class variables
     wheels = 4
@@ -29,13 +30,22 @@ class car():
         mileage = km * avg
         return mileage
 
+# Child Class
 class sports_car(car):
-    def __init__(self, doors:int, roof:bool, spoiler:bool):
+    def __init__(self, name : str, model:int, make:str, doors:int, roof:bool, spoiler:bool):
+        super().__init__(name, model, make) # car.__init__(self, name, model, year) -> another way to initialize constructor
         self.doors = doors
         self.roof = roof
         self.spoiler = spoiler
 
     def info(self):
         return (f'{self.name} is a sports car designed by {self.make} in year {self.model} having {self.doors} doors.')
+
+# Child Class
+class family_car(car):
+    def __init__(self, seats, doors, airbag):
+        pass
     
 cars = car("FXX", 2015, 'Ferrari')
+ferrari = sports_car(2, True, True)
+print(cars.model)
