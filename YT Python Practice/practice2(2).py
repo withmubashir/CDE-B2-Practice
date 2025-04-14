96cfcd3a2871048de8b738c1255be4fb50ba4dc9
withmubashir <kingmubashir018@gmail.com>
2025-04-14 12:27:10 +0500

    practice

--- YT Python Practice/practice2(2).py
+++ YT Python Practice/practice2(2).py
@@ -6,3 +6,16 @@ if(i%2==0):
 else:
     print(i, "is an odd number.")
 
+# 2 WAP to find the greatest of 3 number entered by the user.
+
+u_input1 = int(input("Enter any Number: "))
+u_input2 = int(input("Enter any Number: "))
+u_input3 = int(input("Enter any Number: "))
+
+if(u_input1 > u_input2 and u_input1 > u_input3):
+    print(u_input1, "is the greatest number")
+elif(u_input2 > u_input1 and u_input2 > u_input3):
+    print(u_input2, "is the greatest number")
+else:
+    print(u_input3, "is the greatest number")
+
