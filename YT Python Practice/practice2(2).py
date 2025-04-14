c533810799bca51006809a53b903b1a9693923fb
withmubashir <kingmubashir018@gmail.com>
2025-04-14 12:26:51 +0500

    practice

---
+++ YT Python Practice/practice2(2).py
@@ -0,0 +1,8 @@
+# 1 WAP to check if a number entered by uder is odd or even.
+
+i = int(input("Enter any Number: "))
+if(i%2==0):
+    print(i, "is an even number.")
+else:
+    print(i, "is an odd number.")
+
