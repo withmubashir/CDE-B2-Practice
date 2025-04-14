0325f8b511d12f687cc28ccf874d869a74e2efb9
withmubashir <kingmubashir018@gmail.com>
2025-04-14 07:59:43 +0500

    practice

--- YT Python Practice/practice2.py
+++ YT Python Practice/practice2.py
@@ -1,5 +1,9 @@
-#WAP to input user’s first name & print its length.
+# 1. WAP to input user’s first name & print its length.
 
 first_name = input("Enter Your First Name: ")
 print(len(first_name))
 
+# 2. WAP to find the occurrence of ‘$’ in a String.
+
+str = "abc $ abc $ abc $"
+print(str.count("$"))
