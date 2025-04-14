a25f556e67c8ad4c1cc17c5afa3e0f14eb6f106f
withmubashir <kingmubashir018@gmail.com>
2025-04-14 21:28:01 +0500

    practice

---
+++ YT Python Practice/practice3.py
@@ -0,0 +1,12 @@
+#WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+
+list2 = []
+input1 = input("Enter any Movie Name: ")
+input2 = input("Enter any Movie Name: ")
+input3 = input("Enter any Movie Name: ")
+
+list2.append(input1)
+list2.append(input2)
+list2.append(input3)
+
+print(list2)
