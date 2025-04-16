045c54b9df1ff491a0e5735c658cdb68cac4f17e
withmubashir <kingmubashir018@gmail.com>
2025-04-16 20:41:12 +0500

    practice

---
+++ YT Python Practice/practice4.py
@@ -0,0 +1,11 @@
+#Store following word meanings in a python dictionary :
+#table : “a piece of furniture”,“list of facts & figures”
+#cat : “a small animal”
+
+dict = {
+    "table"  : ("a piece of furniture","list of facts & figures"),
+    "cat" : "a small anima"
+    }
+
+print(type(dict["table"]))
+
