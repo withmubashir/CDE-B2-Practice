13508f3617e63e4eaf44d2773b2fdc6b527082f2
withmubashir <kingmubashir018@gmail.com>
2025-04-16 20:41:43 +0500

    practice

--- YT Python Practice/practice4.py
+++ YT Python Practice/practice4.py
@@ -9,3 +9,11 @@ dict = {
 
 print(type(dict["table"]))
 
+# You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students. 
+# ”python” , “java” , “C++” , “python” , “javascript” , “java” , “python” , “java” , “C++” , “C”
+
+subjects = {
+    "python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"
+}
+
+print(len(subjects))
