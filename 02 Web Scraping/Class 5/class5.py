eb54bb191891b3054fbfbc09f5a5ec89b422f09d
withmubashir <kingmubashir018@gmail.com>
2025-05-10 19:52:56 +0500

    practice

--- 02 Web Scraping/Class 5/class5.py
+++ 02 Web Scraping/Class 5/class5.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.by import By
+# services
 options = Options()
 # options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
 driver.get('https://realpython.github.io/fake-jobs/')
-
-driver.quit()
